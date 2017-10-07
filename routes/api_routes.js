9e23dbfd010d6e1725c81d65b708ae727137f6d1
unknown <luca.canizzo@gmail.com>
2017-10-06 21:30:43 -0400

    Tasks deleting properly from database and inserting into Completed list

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -18,33 +18,34 @@ const api_routing = (app, __dirname)=>{
     });
     // Delete an existing task
     app.get('/deleteTask/:id', (req,res)=>{
+        //Delete task from to_dos
+        let finishedTask = req.body;
         console.log('P A R A M S  I D:\n', req.params.id)
         connection.query("DELETE FROM to_dos WHERE id=?", [req.params.id],(err,result)=>{
             if(err){
                 console.log("ERROR:\n", err);
                 return res.status(500).end();
             } 
-            // else if (result.affectedRows == 0){
-            //     console.log("No such item exists");
-            //     return res.status(404).end();
-            // } 
+            else if (result.affectedRows == 0){
+                console.log("No such item exists");
+                return res.status(404).end();
+            } 
             else {
                 console.log(result);
                 res.json(result).end();
             }
         })
-        // .then(()=>{
-        //     connection.query("INSERT INTO completed (finishedTask) VALUES (?)", [finishedTask], (err, result)=>{
-        //         if(err){
-        //             console.log("ERROR:\n", err);
-        //             return res.status(500).end();
-        //         }
-        //         console.log("R E S U L T:\n", { id: result.insertId });
-        //         res.json({ id: result.insertId });
-        //     });
-        // });
+        //Insert task into completed
+        console.log("Finished Task:\n", finishedTask);            
+        connection.query("INSERT INTO completed (finishedTask) VALUES (?)", [finishedTask], (err, result)=>{
+            if(err){
+                console.log("ERROR:\n", err);
+                return res.status(500).end();
+            }
+            console.log("R E S U L T:\n", { id: result.insertId });
+            res.json({ id: result.insertId });
+        });
     });
-    
 };
 
 module.exports = api_routing;
