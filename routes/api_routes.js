8ad2b9b22a021328f50c2c0eeb272d14f7e24b02
unknown <luca.canizzo@gmail.com>
2017-10-06 22:01:23 -0400

    Mark complete function working properly

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,6 +3,7 @@ const path = require('path');
 const connection = require('../config/connection.js');
 
 const api_routing = (app, __dirname)=>{
+
     // Create a new task
     app.post('/newTask', (req,res)=>{
         let newTask = req.body.newTask;
@@ -16,9 +17,9 @@ const api_routing = (app, __dirname)=>{
             res.json({ id: result.insertId });
         });
     });
+
     // Delete an existing task
     app.get('/deleteTask/:id', (req,res)=>{
-        //Delete task from to_dos
         console.log('P A R A M S  I D:\n', req.params.id)
         connection.query("DELETE FROM to_dos WHERE id=?", [req.params.id],(err,result)=>{
             if(err){
@@ -31,10 +32,29 @@ const api_routing = (app, __dirname)=>{
             } 
             else {
                 console.log(result);
-                res.json(result).end();
+                res.status(200).end();
             }
         })
     });
+
+    // Mark an existing task as complete
+    app.get('/completeTask/:id', (req,res)=>{
+        console.log('P A R A M S  I D:\n', req.params.id)
+        connection.query("UPDATE to_dos SET completed=true WHERE id=?", [req.params.id], (err, result)=>{
+            if(err){
+                console.log("ERROR:\n", err);
+                return res.status(500).end();
+            } 
+            else if (result.changedRows == 0){
+                console.log("No such item exists");
+                return res.status(404).end();
+            } 
+            else {
+                console.log(result);
+                res.status(200).end();
+            }
+        })        
+    })
 };
 
 module.exports = api_routing;
--- schema.sql
+++ schema.sql
@@ -7,5 +7,6 @@ USE to_do_db;
 CREATE TABLE to_dos (
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
     newTask VARCHAR(255) NOT NULL,
+    completed BOOLEAN,
     PRIMARY KEY (id)
 );
