6aec9ea33021d1373ce9951633ebcbb4bcd79b76
unknown <luca.canizzo@gmail.com>
2017-10-06 20:14:49 -0400

    New tasks inserting into data table

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,14 +1,20 @@
 const toDo = require('../data/to_do.js');
 const express = require('express');
 const path = require('path');
+const connection = require('../config/connection.js');
 
 const api_routing = (app, __dirname)=>{
     app.post('/newTask', (req,res)=>{
-        let body = req.body;
-        console.log("R E Q U E S T:\n", body);  
-        toDo.push(body);      
-        res.json(toDo);
-        //this handler would take the posted data (new task) object and append the input string to the toDo array, returning the full array in json
+        let newTask = req.body.newTask;
+        console.log("R E Q U E S T:\n",  req.body);  
+        connection.query("INSERT INTO to_dos (newTask) VALUES (?)", [newTask], (err, result)=>{
+            if(err){
+                console.log("ERROR:\n", err);
+                return res.status(500).end();
+            }
+            console.log("R E S U L T:\n", { id: result.insertId });
+            res.json({ id: result.insertId });
+        })
     });
 };
 
